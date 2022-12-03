d2a04bbb4de518c4f969f7860f0f2b60d159d72a
Brandon Walsh <bmwalshy@gmail.com>
2022-12-02 20:47:56 -0500

    Add Day 1 pt 2

--- day-01/script.py
+++ day-01/script.py
@@ -3,6 +3,8 @@ file = open('input.txt', 'r')
 lines: list[str] = file.readlines()
 file.close()
 
+## Part 1
+
 # We definitely don't need to store all the totals in an array
 # Just keeping track of the highest in an array would suffice (and save space)
 calorie_count_per_elf: list[int] = []
@@ -22,5 +24,28 @@ for calories in lines:
   # Add local total until we hit newline
   local_total += int(calories)
 
+# Sloppy, but account for the input not ending in a new line
+calorie_count_per_elf.append(local_total)
+
 # O(n)
 print(f"Elf with highest number of calories: {max(calorie_count_per_elf)}")
+
+## Part 2
+
+# Good thing we saved the elves' calories in an array!
+# Because we could have saved the highest 3 in variables 
+# and kept track of them as we looped through the list,
+# but no thank you.
+
+highest_three = 0
+
+print(sorted(calorie_count_per_elf))
+# O(3 * n) => O(n)
+for i in range(0,3):
+  local_max = max(calorie_count_per_elf) # O(n)
+  print(local_max)
+  calorie_count_per_elf.remove(local_max) # O(n)
+  highest_three += local_max
+  
+
+print(f"Highest three elves' calories: {highest_three}")
