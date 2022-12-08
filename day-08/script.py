7637793caec0d03a961c4f7782f5a161952bc1a3
Brandon Walsh <bmwalshy@gmail.com>
2022-12-08 11:36:03 -0500

    Day 8 pt 2

--- day-08/script.py
+++ day-08/script.py
@@ -55,3 +55,47 @@ def part1():
   print(visible_trees)
 
 part1()
+
+def part2():
+  highest_scenic_score = 0
+
+  for i in range(0, len(board)):
+    for j in range(0, len(board[0])):
+      if i - 1 < 0 or i + 1 >= len(board) or j - 1 < 0 or j + 1 >= len(board[0]):
+        continue
+        
+      left_score = 0
+      # Check left
+      for a in range(j - 1, -1, -1):
+        left_score += 1
+        if board[i][a] >= board[i][j]:
+          break
+          
+      # Check right
+      right_score = 0
+      for a in range(j + 1, len(board[0])):
+        right_score += 1
+        if board[i][a] >= board[i][j]:
+          break
+
+      # check Up
+      up_score = 0
+      for a in range(i - 1, -1, -1):
+        up_score += 1
+        if board[a][j] >= board[i][j]:
+          break
+
+      # check down
+      down_score = 0
+      for a in range(i + 1, len(board[0])):
+        down_score += 1
+        if board[a][j] >= board[i][j]:
+          break
+
+      local_score = up_score * down_score * left_score * right_score
+      #print((i,j), up_score, down_score, left_score, right_score)
+      highest_scenic_score = max(highest_scenic_score, local_score)
+
+  print(highest_scenic_score)
+
+part2()
