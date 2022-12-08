73c89599b6561bf30db18625ef5608a9b5e010f9
Brandon Walsh <bmwalshy@gmail.com>
2022-12-08 02:18:14 -0500

    Comments woo

--- day-07/script.py
+++ day-07/script.py
@@ -37,6 +37,7 @@ class Node:
 
 head = current = Node('/', None)
 
+# Build a tree :)
 # For each line
 for index, line in enumerate(lines):
   if index == 0:
@@ -55,10 +56,12 @@ for index, line in enumerate(lines):
     if item[2] == 'c':
       dir_name = item.split(' ')[2]
 
+      # Go up a dir
       if dir_name == '..':
         #print(f"update current from {current.dir_name} to {current.parent.dir_name}")
         current = current.parent
       else:
+        # Else find existing dir
         #print(f"update current from {current.dir_name} to {current.find_subdir(dir_name).dir_name}")
         current = current.find_subdir(dir_name)
 
@@ -82,9 +85,18 @@ for index, line in enumerate(lines):
 # Go through tree and total up the sizes of each dir so their 
 # sizes are accurate and include the size of their subdirs
 to_find = [head]
+
+
+# okay this is named horribly
+# already_seen will keep track of nodes who's size has been added to its parent's
 already_seen = set()
 
+# lets dfs our way through this baby!
+# How this works is we find leaves in the tree, then adds the current node's value to its parent, then puts its 
+# parent back at the top of the stack. Then we check for the current node if we've already gone through all the children.
+# If so, we add the current node's value to its parent. Else, we go through all the children that we haven't visited yet
 while to_find:
+  # for quick references
   current_node = to_find.pop()
   parent = current_node.parent
 
@@ -93,6 +105,7 @@ while to_find:
   if current_node in already_seen:
     continue
 
+  # leaf scenario
   if len(current_node.subdirs) == 0: 
     parent.size += current_node.size
     already_seen.add(current_node)
@@ -100,31 +113,41 @@ while to_find:
       to_find.append(parent.parent)
     continue
 
+  # Another poorly named variable
+  # this flag marks whether we've visited all the current node's children yet already
   all_seen = True
 
+  # Loop through the current node's children (the subdirectories)
   for subdir_name in current_node.subdirs:
     subdir = current_node.subdirs[subdir_name]
+
+    # Skip if already visited
     if subdir in already_seen:
       continue
 
+    # We get here if haven't visited this node
+    # So we mark all_seen as False because we haven't visited all the children of this node yet
     all_seen = False
     skip = False
 
+    # Dirty way to make sure I don't add duplicates on the stack
     for x in to_find:
       if x.dir_name == subdir_name:
         skip = True
         break
 
-    if not skip and subdir not in already_seen:
+    # If this child is not already in the to_find stack add it to the stack
+    if not skip:
       to_find.append(subdir)
 
-  # start backtracking i hope
-  #if len(current_node.subdirs) == 0: 
+  # If all the children have been visited
+  # add the current value to the parent
+  # And then visit the parent
   if all_seen:
     parent.size += current_node.size
-    #parent.subdirs.pop(current_node.dir_name)
     already_seen.add(current_node)
     if parent.parent:
+      # to_find.append(parent.parent) works too?? this line is more work probably?
       to_find.append(parent)
 
 total_size = head.size
@@ -135,9 +158,12 @@ def part1():
   sum = 0
 
   # If I knew how to backtrack properly I wouldn't have to DFS twice :(
+  # Maybe two DFS is good?
   while to_find:
     current_node = to_find.pop()
     #print(f"Current Node: {current_node.path}, subdirs: {current_node.subdirs.keys()}, size: {current_node.size}")
+
+    # Sum up nodes (dirs) who's size is less than this
     if current_node.size < 100000:
       sum += current_node.size
 
@@ -147,12 +173,13 @@ def part1():
 
   print(sum)
 
-#part1()
+part1()
 
 def part2():
   to_find = [head]
   sizes = []
 
+  # Add all sizes to an array
   while to_find:
     current_node = to_find.pop()
     sizes.append(current_node.size)
@@ -161,9 +188,10 @@ def part2():
       subdir = current_node.subdirs[subdir_name]
       to_find.append(subdir)
 
+  # sort array
   sizes.sort()
 
-  print(sizes)
+  # calculate smallest dir we could delete that would give us at least 30000000 space 
   free_space = 70000000 - total_size # 27919656
   for size in sizes:
     if size + free_space >= 30000000:
