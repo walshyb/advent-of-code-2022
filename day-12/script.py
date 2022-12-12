75a6988270ac28d5de9b49357fe46264d2f37e36
Brandon Walsh <bmwalshy@gmail.com>
2022-12-12 13:04:11 -0500

    Cleanup

--- day-12/script.py
+++ day-12/script.py
@@ -9,10 +9,12 @@ file.close()
 board = []
 start_pos, end_pos = (0,0), (0,0)
 
+# Make the board
 for line in lines:
   row = list(line.strip())
   board.append(list(row))
 
+  # Little hack to assist with checking neighbors when processing the board
   if 'S' in row: 
     start_pos = (len(board) - 1, row.index('S'))
     board[start_pos[0]][start_pos[1]] = 'a'
@@ -21,49 +23,58 @@ for line in lines:
     end_pos = (len(board) - 1, row.index('E'))
     board[end_pos[0]][end_pos[1]] = 'z'
 
-# bfs bfs bfs 
-
+# Helper to return None for out of bounds checks
 def fetch(x, y):
   if x >= len(board) or x < 0 or y < 0 or y >= len(board[0]):
     return None
   return board[x][y]
 
-queue = deque()
-seen = set({(start_pos, 0)})
-
-queue.append((start_pos, 0))
 
 def solve(any_start):
+  # Make a queue for our BFS (Breadth first search)
+  queue = deque()
+  queue.append((start_pos, 0))
+  seen = set({(start_pos, 0)})
+
+  # bfs bfs bfs 
   while queue:
     (x,y), steps = queue.popleft()
+
+    # Get current character
     current_height = board[x][y]
 
+    # If we're at the end, print the number of steps
     if (x,y) == end_pos:
       print(steps)
       break
 
+    # Check which neighbors we can go to
     neighbors = [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
 
+    # For every neighbor candidate
     for neighbor_pos in neighbors:
       neighbor_x, neighbor_y = neighbor_pos
       neighbor = fetch(neighbor_x, neighbor_y)
 
+      # This is for part 2
+      # This makes it so we can track the shortest path
+      # from any elevation 'a' and not just the starting position
       step_increase = 1
-
       if any_start and current_height == 'a' and neighbor == 'a':
         step_increase = 0
 
+      # Ignore if out of bounds or already seen
       if not neighbor or neighbor_pos in seen:
         continue
 
       # If the neighbor is 1 elevation higher, it's valid
-      # If the neighbor is at max 2 levels lower, it's valid
       if ord(neighbor) - ord(current_height) == 1:
         queue.append(((neighbor_x, neighbor_y), steps + step_increase))
         seen.add(neighbor_pos)
+      # If the neighbor is at max 2 levels lower, it's valid
       elif ord(current_height) - ord(neighbor) >= 0 and ord(current_height) - ord(neighbor) < 3: 
         queue.append(((neighbor_x, neighbor_y), steps + step_increase))
         seen.add(neighbor_pos)
 
-#solve(False) # part 1
+solve(False)  # part 1
 solve(True)   # part 2
