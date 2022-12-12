238b9044cea5d5ce615743f79e22eae6d0607821
Brandon Walsh <bmwalshy@gmail.com>
2022-12-11 22:51:02 -0500

    Day 11 pt2

--- day-11/script.py
+++ day-11/script.py
@@ -5,6 +5,7 @@ file.close()
 
 monkeys = {}
 current_monkey_info = {}
+global_divide = 1
 
 # Parse input
 for index, line in enumerate(lines):
@@ -23,7 +24,9 @@ for index, line in enumerate(lines):
     current_monkey_info['operation'] = line.split('= ')[1].strip()
   
   if line.find('Test: ') != -1:
-    current_monkey_info['divide'] =  int(line.split(' ')[5].strip())
+    divide = int(line.split(' ')[5].strip())
+    current_monkey_info['divide'] = divide 
+    global_divide *= divide
   
   if line.find('true') != -1:
     current_monkey_info['true'] = line.split(' ')[-1].strip()
@@ -31,31 +34,38 @@ for index, line in enumerate(lines):
   if line.find('false') != -1:
     current_monkey_info['false'] = line.split(' ')[-1].strip()
 
-def part1():
+#print(monkeys)
+#print(global_divide)
+def solve(rounds, divide_worry):
   # For 20 rounds
-  for i in range(0, 20):
+  for i in range(0, rounds):
     # For each monkey
     for j in range(0, len(monkeys)):
-      current_monkey = monkeys[str(j)]
-      #print(current_monkey)
-      for k in range(0, len(current_monkey['items'])):
-        old = current_monkey['items'][k]        # old worry level
-        new = eval(current_monkey['operation']) # new worry level
 
-        new_worry = new // 3
-        if new_worry % current_monkey['divide'] == 0:
-          monkeys[current_monkey['true']]['items'].append(new_worry)
-        else:
-          monkeys[current_monkey['false']]['items'].append(new_worry)
+      current_monkey = monkeys[str(j)]
+
+      for k in range(0, len(current_monkey['items'])):
+        old = current_monkey['items'][k]        # old worry level, used in eval
+        new = eval(current_monkey['operation']) # new worry level
         
-        current_monkey['inspections'] += 1
-      
+        if divide_worry:
+          new = new // 3
+
+          if new % current_monkey['divide'] == 0:
+            monkeys[current_monkey['true']]['items'].append(new)
+          else:
+            monkeys[current_monkey['false']]['items'].append(new)
+        else:
+          if new % current_monkey['divide'] == 0:
+            monkeys[current_monkey['true']]['items'].append(new % global_divide)
+          else:
+            monkeys[current_monkey['false']]['items'].append(new % global_divide)
+        
+      current_monkey['inspections'] += len(current_monkey['items'])
       current_monkey['items'] = []
 
-  print(monkeys)
-
   # Get two most active monkeys
-  most_inspections = second_most_inspections = monkeys['0']['inspections']
+  most_inspections = second_most_inspections = 0
 
   for i in range(0, len(monkeys)):
     current = monkeys[str(i)]
@@ -70,4 +80,5 @@ def part1():
 
   print(most_inspections * second_most_inspections)
 
-part1()
+# solve(20, True) # Part 1
+solve(10000, False) # Part 2
